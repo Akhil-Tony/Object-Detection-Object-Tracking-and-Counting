ca5bcb0b973b1257d61a102ca2378734dfc7061a
Akhil Tony Xavier <72643926+Akhil-Tony@users.noreply.github.com>
2022-08-28 02:47:00 +0530

    Add files via upload

---
+++ object_tracker.py
@@ -0,0 +1,170 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import cv2
+import numpy as np
+import tensorflow as tf
+from yolov3.utils import Load_Yolo_model, image_preprocess, postprocess_boxes, nms, draw_bbox, read_class_names
+from yolov3.configs import *
+import time
+
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from deep_sort import generate_detections as gdet
+
+from deep_sort.counter import get_count
+
+video_path   = "video_3.mp4"
+
+def Object_tracking(Yolo, video_path, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.6, iou_threshold=0.45, rectangle_colors='', Track_only = []):
+
+    # Definition of the parameters
+    max_cosine_distance = 0.7
+    nn_budget = None
+    
+    #initialize deep sort object
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    times, times_2 = [], []
+
+    if video_path:
+        vid = cv2.VideoCapture(video_path) # detect on video
+    else:
+        vid = cv2.VideoCapture(0) # detect from webcam
+
+    # by default VideoCapture returns float instead of int
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    codec = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_path, codec, fps, (width, height)) # output_path must be .mp4
+
+    NUM_CLASS = read_class_names(CLASSES)
+    key_list = list(NUM_CLASS.keys()) 
+    val_list = list(NUM_CLASS.values())
+    
+    # For counting
+    object_set = set()
+    object_count = {"car":0,"truck":0,"bus":0,"bike":0,"person":0}
+    
+    while True:
+        _, frame = vid.read()
+
+        try:
+            original_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            original_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2RGB)
+        except:
+            break
+        
+        image_data = image_preprocess(np.copy(original_frame), [input_size, input_size])
+        #image_data = tf.expand_dims(image_data, 0)
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+        t1 = time.time()
+        if YOLO_FRAMEWORK == "tf":
+            pred_bbox = Yolo.predict(image_data)
+        elif YOLO_FRAMEWORK == "trt":
+            batched_input = tf.constant(image_data)
+            result = Yolo(batched_input)
+            pred_bbox = []
+            for key, value in result.items():
+                value = value.numpy()
+                pred_bbox.append(value)
+        
+        #t1 = time.time()
+        #pred_bbox = Yolo.predict(image_data)
+        t2 = time.time()
+        
+        pred_bbox = [tf.reshape(x, (-1, tf.shape(x)[-1])) for x in pred_bbox]
+        pred_bbox = tf.concat(pred_bbox, axis=0)
+
+        bboxes = postprocess_boxes(pred_bbox, original_frame, input_size, score_threshold)
+        bboxes = nms(bboxes, iou_threshold, method='nms')
+
+        # extract bboxes to boxes (x, y, width, height), scores and names
+        boxes, scores, names = [], [], []
+        for bbox in bboxes:
+            if len(Track_only) !=0 and NUM_CLASS[int(bbox[5])] in Track_only or len(Track_only) == 0:
+                boxes.append([bbox[0].astype(int), bbox[1].astype(int), bbox[2].astype(int)-bbox[0].astype(int), bbox[3].astype(int)-bbox[1].astype(int)])
+                scores.append(bbox[4])
+                names.append(NUM_CLASS[int(bbox[5])])
+
+        # Obtain all the detections for the given frame.
+        boxes = np.array(boxes) 
+        names = np.array(names)
+        scores = np.array(scores)
+        features = np.array(encoder(original_frame, boxes))
+        #(center x, center y, aspect ratio,height)
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(boxes, scores, names, features)]
+        
+        # Pass detections to the deepsort object and obtain the track information.
+        tracker.predict()
+        tracker.update(detections)
+
+        # Obtain info from the tracks
+        tracked_bboxes = []
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 5:
+                continue 
+            bbox = track.to_tlbr() # Get the corrected/predicted bounding box
+            
+            class_name = track.get_class() #Get the class name of particular object
+            tracking_id = track.track_id # Get the ID for the particular track
+            index = key_list[val_list.index(class_name)] # Get predicted object index by object name
+            tracked_bboxes.append(bbox.tolist() + [tracking_id, index]) # Structure data, that we could use it with our draw_bbox function
+
+        # draw detection on frame
+        image = draw_bbox(original_frame, tracked_bboxes, CLASSES=CLASSES, tracking=True)
+        
+        
+        # Counting
+        if video_path == 'video_1.mp4':
+            scale = .5
+        else:
+            scale = .2
+        ob_count,ob_set = get_count(original_frame, tracked_bboxes, object_set, object_count, scale=scale)
+        object_set = ob_set
+        object_count = ob_count
+        
+        cv2.putText(image, "car: "+str(object_count["car"]), (10, 320), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+        cv2.putText(image, "truck: "+str(object_count["truck"]), (10, 350), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+        cv2.putText(image, "bus: "+str(object_count["bus"]), (10, 380), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+        cv2.putText(image, "bike: "+str(object_count["bike"]), (10, 410), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+        cv2.putText(image, "person: "+str(object_count["person"]), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+        
+        # Counting
+        frame_height,frame_width,_ = original_frame.shape
+        cv2.line(image,(0,int(frame_height*scale)),(frame_width,int(frame_height*scale)),(255,0,0),1)
+        
+        t3 = time.time()
+        times.append(t2-t1)
+        times_2.append(t3-t1)
+        
+        times = times[-20:]
+        times_2 = times_2[-20:]
+
+        ms = sum(times)/len(times)*1000
+        fps = 1000 / ms
+        fps2 = 1000 / (sum(times_2)/len(times_2)*1000)
+        
+        image = cv2.putText(image, "Time: {:.1f} FPS".format(fps), (0, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+
+        # draw original yolo detection     
+#         image = draw_bbox(image, bboxes, CLASSES=CLASSES, show_label=False, rectangle_colors=(0,255,0), tracking=True)
+
+        if output_path != '': out.write(image)
+        if show:
+            cv2.imshow('output', image)
+            
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                break
+            
+    cv2.destroyAllWindows()
+
+
+yolo = Load_Yolo_model()
+Object_tracking(yolo, video_path, "track_1.mp4", input_size=YOLO_INPUT_SIZE, show=True, iou_threshold=0.1, rectangle_colors=(255,0,0), Track_only = ["car","truck","bus","bike","person"])
