e20abd8c605adec30b5bb44715af2be061f8a871
Akhil Tony Xavier <72643926+Akhil-Tony@users.noreply.github.com>
2022-08-28 10:34:53 +0530

    Update counter.py

--- deep_sort/counter.py
+++ deep_sort/counter.py
@@ -1,4 +1,3 @@
-
 def read_class_names(class_file_name):
     # loads class name from a file
     names = {}
@@ -7,23 +6,30 @@ def read_class_names(class_file_name):
             names[ID] = name.strip('\n')
     return names
 
-YOLO_COCO_CLASSES = read_class_names("deep_sort/yolo_classes/coco.names")
-
-def get_count(image, bboxes, object_set, object_count, scale, CLASSES=YOLO_COCO_CLASSES):
-    frame_height,_,_ = image.shape
-    y_line = int(frame_height * scale)
-    for bbox in bboxes:
-        class_ind = int(bbox[5])
-        class_name = CLASSES[class_ind]
-        id_num = bbox[4]
-        y_center = (bbox[1] + bbox[3])/2
-        set_element = class_name+str(id_num)
-        if y_center <= y_line:
-            if set_element not in object_set:
-                object_set.add(set_element)
-                object_count[class_name] += 1
+class Count:
+    CLASSES = None
+    scale = None
     
-    return object_count,object_set
+    def __init__(self, scale):
+        self.CLASSES = read_class_names("deep_sort/yolo_classes/coco.names")
+        self.scale = scale
+        
+    def get_count(self,image_shape, bboxes, object_set, object_count):
+        frame_height,_,_ = image_shape
+        y_line = int(frame_height * self.scale)
+        for bbox in bboxes:
+            class_ind = int(bbox[5])
+            class_name = self.CLASSES[class_ind]
+            id_num = bbox[4]
+            y_center = (bbox[1] + bbox[3])/2
+            set_element = class_name+str(id_num)
+            if y_center <= y_line:
+                if set_element not in object_set:
+                    object_set.add(set_element)
+                    object_count[class_name] += 1
+
+        return object_count,object_set
+
 
         
 
